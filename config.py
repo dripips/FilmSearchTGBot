7dc031bd167f552cb91b2cd1a7a0148fb516383b
dripips <66335115+dripips@users.noreply.github.com>
2023-10-12 03:12:28 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Токен вашего Telegram-бота
+TELEGRAM_BOT_TOKEN = ''
+
+# Токен API для доступа к данным фильмов
+API_TOKEN = ''
+
+# URL для запросов к API
+API_URL = 'https://apivb.info/api/videos.json'
